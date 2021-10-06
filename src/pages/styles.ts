32da2e1b84a0eee7488e1ff39e1cd7dcde5f05ef
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-06 13:57:54 -0300

    Add: index style

---
+++ src/pages/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    max-width: 1120px;
+    width: 100%;
+    margin: 4rem auto 0;
+    
+    
+    @media(max-width: 1120px) {
+        padding: 0 .8rem;
+    }
+`
