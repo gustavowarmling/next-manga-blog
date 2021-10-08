d4442f182b08d96bffe21b1a85623dca1003d12c
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-08 12:46:40 -0300

    Refactor: changed Likes style

--- src/ui/Likes/styles.ts
+++ src/ui/Likes/styles.ts
@@ -2,11 +2,5 @@ import styled from "styled-components";
 
 export const LikesStyle = styled.span`
     display: flex;
-    align-items: center;
-    font-size: 1.3rem;
-
-    svg {
-        font-size: 2rem;
-        margin-left: 0.4rem;
-    }
+    justify-content: center;
 `
