54999303d4270ecfc675922e027a67609d21e127
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-08 13:37:40 -0300

    Add: added tooltip in Tag component

--- src/ui/Tag/index.tsx
+++ src/ui/Tag/index.tsx
@@ -1,13 +1,26 @@
 import { StyledTag } from "./styles";
+import ReactTooltip from 'react-tooltip';
 
 interface TagProps {
     children: String;
+    id: string;
 }
 
-export default function Tag({children}: TagProps) {
+export default function Tag({children, id}: TagProps) {
     return (
-        <StyledTag>
-            {children}       
-        </StyledTag>
+        <>
+            <StyledTag 
+            data-tip="Tag description" 
+            data-for={id}
+            >
+                {children}       
+            </StyledTag>
+            <ReactTooltip 
+                id={id} 
+                effect="solid" 
+                backgroundColor="var(--blue-500)" 
+                textColor="var(--subtitle)"
+            />
+        </> 
     )
 }
--- src/ui/Tag/styles.ts
+++ src/ui/Tag/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledTag = styled.span`
     
         background: var(--blue-900);
-        padding: 0.5rem;
+        padding: 0.5rem 0.8rem;
         border-radius: 1rem;
         border: 2px solid var(--blue-300);
     
