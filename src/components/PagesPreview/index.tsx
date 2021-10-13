6e88e673bdbadb7812488621f6fad20c3e218cd7
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-13 13:57:33 -0300

    Add: PagesPreview component

---
+++ src/components/PagesPreview/index.tsx
@@ -0,0 +1,47 @@
+import { PagesContainer, PagesPreviewContainer } from "./styles"
+
+export function PagesPreview() {
+    return (
+        <PagesPreviewContainer>
+            <h2>Pages</h2>
+            <PagesContainer>
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+                <img 
+                    src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg" 
+                    alt="Battle Royale Cover" 
+                />
+            </PagesContainer>
+        </PagesPreviewContainer>
+    )
+}
