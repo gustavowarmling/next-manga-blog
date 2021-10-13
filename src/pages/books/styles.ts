2ed98a27324b6765c3b256432bf9288807ebcca2
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-13 13:56:44 -0300

    Add: books page

---
+++ src/pages/books/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    max-width: 1120px;
+    margin: 4rem auto 0;
+
+    @media(max-width: 1120px) {
+        padding: 0 .8rem;
+    }
+`
+
+export const ContentContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    gap: 24px;
+
+    @media(max-width: 650px) {
+        grid-template-columns: 2fr 3fr;
+    }
+`
