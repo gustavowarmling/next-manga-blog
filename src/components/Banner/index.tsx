c5049019b8a59108bab74efe7bf6d32b28ff4320
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-13 13:57:03 -0300

    Add: Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,17 @@
+import { BannerContainer } from "./styles"
+
+type BannerProps = {
+    title: string;
+    image: string;
+  }
+
+export function Banner({image, title}: BannerProps) {
+    return(
+        <BannerContainer>
+            <img 
+                src={image} 
+            />
+            <h1>{title}</h1>
+        </BannerContainer>
+    )
+}
---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const BannerContainer = styled.div`
+    position: relative;
+    height: 20rem;
+    margin-bottom: 16px;
+
+    img {
+        width: 100%;
+        height: 20rem;
+        filter: blur(4px);
+    }
+
+    h1 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        text-align:center;
+        transform: translateX(-50%) translateY(-50%);
+        font-size: 3rem;
+    }
+`
