2d5d1b95531e35d073368d03d92ee751c5dce831
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-14 12:49:00 -0300

    Add: BookInfo component

--- src/components/BookInfo/index.tsx
+++ src/components/BookInfo/index.tsx
@@ -1,9 +1,41 @@
-import { InfoContainer } from "./style";
+import Link from "next/link";
+import { Tag } from "../../ui/Tag";
+import { InfoContainer, Info, TagsContainer, BookTags } from "./style";
 
 export function BookInfo() {
   return (
     <InfoContainer>
-      <h2>Info</h2>
+      <Info>
+        <span>Synopsis</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          perspiciatis asperiores maiores fuga, sunt accusantium, nihil tempore
+          recusandae voluptatum illum repellendus tenetur aspernatur ullam. Sunt
+          reiciendis pariatur quis expedita nemo?
+        </p>
+        <span>Collection:</span>
+        <Link href="/">
+          <a>one-shot</a>
+        </Link>
+
+        <span>Parody:</span>
+        <Link href="/">
+          <a>original</a>
+        </Link>
+      </Info>
+      <TagsContainer>
+        <p>Tags:</p>
+        <BookTags>
+          <Tag title="teste" />
+          <Tag title="teste" />
+          <Tag title="teste" />
+          <Tag title="teste" />
+          <Tag title="teste" />
+          <Tag title="teste" />
+          <Tag title="teste" />
+          <Tag title="teste" />
+        </BookTags>
+      </TagsContainer>
     </InfoContainer>
   );
 }
--- src/components/BookInfo/style.ts
+++ src/components/BookInfo/style.ts
@@ -2,6 +2,43 @@ import styled from "styled-components";
 
 export const InfoContainer = styled.div`
   background-color: var(--blue-500);
-  padding: 1rem;
+  padding: 1.4rem;
   border-radius: 0.4rem;
+`;
+
+export const Info = styled.div`
+  span {
+    color: var(--subtitle);
+    display: block;
+    margin-bottom: 4px;
+  }
+
+  p {
+    color: var(--description);
+    margin-left: 8px;
+    margin-bottom: 16px;
+  }
+
+  a {
+    color: var(--description);
+    display: block;
+    margin-left: 8px;
+    margin-bottom: 16px;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const TagsContainer = styled.div`
+  p {
+    margin-bottom: 8px;
+  }
+`;
+
+export const BookTags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.8rem;
 `;
