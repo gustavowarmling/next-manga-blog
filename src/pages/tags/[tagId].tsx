6889724d1f70bcc366330b236d758e355a557f5b
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-14 12:46:47 -0300

    Add: tags page

---
+++ src/pages/tags/[tagId].tsx
@@ -0,0 +1,19 @@
+import { Banner } from "../../components/Banner";
+import { TagInfo } from "../../components/TagInfo";
+import { BooksPreview } from "../../components/BooksPreview";
+import { Container, ContentContainer } from "./styles";
+
+export default function Tags() {
+  return (
+    <Container>
+      <Banner
+        title="Dystopian"
+        image="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg"
+      />
+      <ContentContainer>
+        <TagInfo />
+        <BooksPreview />
+      </ContentContainer>
+    </Container>
+  );
+}
