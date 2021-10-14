f8bc419ba3028b9734900ba7753f4eca9fa314b5
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-14 08:54:06 -0300

    Fix: turned Tag component into a link

--- src/ui/Tag/index.tsx
+++ src/ui/Tag/index.tsx
@@ -1,27 +1,27 @@
+import Link from "next/link";
 import { StyledTag } from "./styles";
-import ReactTooltip from 'react-tooltip';
+import ReactTooltip from "react-tooltip";
 
 interface TagProps {
-    title: string;
+  title: string;
 }
 
 export function Tag({ title }: TagProps) {
-    return (
-        <>
-            <StyledTag 
-            data-tip="Tag description" 
-            data-for={title}
-            >
-                {title}       
-            </StyledTag>
-            <ReactTooltip 
-                id={title} 
-                effect="solid" 
-                backgroundColor="var(--blue-500)" 
-                textColor="var(--subtitle)"
-                border={true}
-                borderColor="var(--blue-300)"
-            />
-        </> 
-    )
+  return (
+    <Link href="">
+      <a>
+        <StyledTag data-tip="Tag description" data-for={title}>
+          {title}
+        </StyledTag>
+        <ReactTooltip
+          id={title}
+          effect="solid"
+          backgroundColor="var(--blue-500)"
+          textColor="var(--subtitle)"
+          border={true}
+          borderColor="var(--blue-300)"
+        />
+      </a>
+    </Link>
+  );
 }
--- src/ui/Tag/styles.ts
+++ src/ui/Tag/styles.ts
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 export const StyledTag = styled.span`
-    
-        background: var(--blue-900);
-        padding: 0.5rem 0.8rem;
-        border-radius: 1rem;
-        border: 2px solid var(--blue-300);
-    
-`
+  display: block;
+  background: var(--blue-900);
+  padding: 0.5rem 0.8rem;
+  border-radius: 1rem;
+  border: 2px solid var(--blue-300);
+`;
