24b03874ef73bcbc5c59e9f8372ac3db849ecfc4
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-14 08:55:11 -0300

    Fix: turned Card component into a Link

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -8,49 +8,54 @@ import {
   Info,
   LikePositioning,
 } from "./styles";
+import Link from "next/link";
 
 export function Card() {
   return (
-    <Container>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg"
-        alt="Battle Royale Cover"
-      />
+    <Link href="http://localhost:3000/books/1">
+      <a>
+        <Container>
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/31ED3F1GGRL.jpg"
+            alt="Battle Royale Cover"
+          />
 
-      <Description className="hideTags">
-        <Info>
-          <p>Title</p>
-          <span>Battle Royale</span>
-        </Info>
+          <Description className="hideTags">
+            <Info>
+              <p>Title</p>
+              <span>Battle Royale</span>
+            </Info>
 
-        <Info>
-          <p>Author</p>
-          <span>Koushun Takami</span>
-        </Info>
+            <Info>
+              <p>Author</p>
+              <span>Koushun Takami</span>
+            </Info>
 
-        <Info>
-          <p>Publication Date</p>
-          <span>01/04/1999</span>
-        </Info>
+            <Info>
+              <p>Publication Date</p>
+              <span>01/04/1999</span>
+            </Info>
 
-        <Info>
-          <p>Tags</p>
-          <TagList>
-            <Tag title="Horror" />
-            <Tag title="Dystopian" />
-            <Tag title="Suspense" />
-          </TagList>
-        </Info>
+            <Info>
+              <p>Tags</p>
+              <TagList>
+                <Tag title="Horror" />
+                <Tag title="Dystopian" />
+                <Tag title="Suspense" />
+              </TagList>
+            </Info>
 
-        <LikePositioning>
-          <Likes quantity={2300} />
-        </LikePositioning>
-      </Description>
+            <LikePositioning>
+              <Likes quantity={2300} />
+            </LikePositioning>
+          </Description>
 
-      <CardFooter>
-        <p>Battle Royale</p>
-        <span>Koushun Takami</span>
-      </CardFooter>
-    </Container>
+          <CardFooter>
+            <p>Battle Royale</p>
+            <span>Koushun Takami</span>
+          </CardFooter>
+        </Container>
+      </a>
+    </Link>
   );
 }
