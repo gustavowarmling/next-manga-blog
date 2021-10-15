875685bd5e105a6257a0170d6fe4ca03475b17dd
Gustavo Warmling <gustavowarmling@gmail.com>
2021-10-15 00:24:08 -0300

    Add: Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Footer() {
+  return (
+    <Container>
+      <span>Projected and designed by Gustavo & Mateus</span>
+    </Container>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  margin-top: auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--blue-500);
+
+  padding: 2rem 0.5rem;
+`;
